287e84345fca225cd3b46dce28126852bcbbdc53
ECHO-FOLLOW-HEART <pengyt19890703@163.com>
2017-01-04 23:12:07 +0800

    1

--- src/com/leetcode/twosum1/Solution.java
+++ src/com/leetcode/twosum1/Solution.java
@@ -1,11 +1,36 @@
 package com.leetcode.twosum1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 1 Two Sum
  * Created by pengyt on 2017/1/4.
  */
 public class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        return null;
+    public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        int len = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < len; i++) {
+            if(map.isEmpty())
+                map.put(nums[i], i);
+            else {
+                if(map.containsKey(target - nums[i])) {
+                    result[0] = map.get(target - nums[i]);
+                    result[1] = i;
+                    break;
+                } else {
+                    map.put(nums[i], i);
+                }
+            }
+        }
+        return result;
     }
+
+//    public static void main(String[] args) {
+//        int[] numbers = {3,2,4};
+//        int[] result = twoSum(numbers, 6);
+//        System.out.print(result[0] + " " + result[1]);
+//    }
 }
