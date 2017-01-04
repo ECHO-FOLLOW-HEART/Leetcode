78871241ccf22b5cfd7f94758852ac7d15e88e9e
ECHO-FOLLOW-HEART <pengyt19890703@163.com>
2017-01-04 21:23:09 +0800

    init

---
+++ src/com/leetcode/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.leetcode;
+
+/**
+ * Hello World
+ * Created by pengyt on 2017/1/4.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
---
+++ src/com/leetcode/twosum1/Solution.java
@@ -0,0 +1,11 @@
+package com.leetcode.twosum1;
+
+/**
+ * 1 Two Sum
+ * Created by pengyt on 2017/1/4.
+ */
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        return null;
+    }
+}
